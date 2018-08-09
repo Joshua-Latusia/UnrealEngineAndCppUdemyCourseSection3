0c950f504efc4d1693bee643b9dbd2992ad3bb67
Joshua Latusia <jcdslatu@avans.nl>
2018-08-09 22:34:05 +0200

    Add objects to scene and print their names to the console

---
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Gameframework/Actor.h"
+#include "PositionReporter.h"
+
+
+// Sets default values for this component's properties
+UPositionReporter::UPositionReporter()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReporter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FString name = GetOwner()->GetName();
+	UE_LOG(LogTemp, Error, TEXT("Hello my name is %s"), *name);
+	
+	// ...
+	
+}
+
+
+// Called every frame
+void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
