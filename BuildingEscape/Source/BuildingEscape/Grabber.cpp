0573e652b3924331f8d0060485a5eeb3b4374002
Joshua Latusia <jcdslatu@avans.nl>
2018-08-25 17:07:33 +0200

    Added BluePrint Gamemode with Blueprint pawn so that components can be added to the player + added (empty) grabber component to the player

---
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -0,0 +1,34 @@
+// Copyright Joshua Latusia 2018
+
+#include "Grabber.h"
+#include "Gameframework/Actor.h"
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString Name = GetOwner()->GetName();
+	UE_LOG(LogTemp, Error, TEXT("%s : Grabber is working"), *Name);
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
